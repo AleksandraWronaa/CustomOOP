284a2659c487eea233e724552e3fc4e29350a84d
Aleksandra <149684219+AleksandraWronaa@users.noreply.github.com>
2024-01-19 12:07:08 +0100

    Add files via upload

--- CustomOOP/CustomOOP.cpp
+++ CustomOOP/CustomOOP.cpp
@@ -4,13 +4,15 @@
 
 using namespace std;
 
-const int boardSizeX = 50, boardSizeY = boardSizeX / 2;   //board size
+const int boardSizeX = 25, boardSizeY = boardSizeX / 2;   //board size
 bool gameStop;
-int snakeX, snakeY, appleX, appleY;
+int snakeX, snakeY, appleX, appleY, score;
 int snakebodyX[boardSizeX*boardSizeY], snakebodyY[boardSizeX * boardSizeY];
 int bodynumber=0;
 enum Movement {UP, DOWN, LEFT, RIGHT, STOP}; //tracks the direction in which is snake going
 Movement direction;
+
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 
 void changeColor(int desiredColor) 
 {
@@ -22,7 +24,7 @@ void Start()
     srand(time(NULL));
     gameStop = false;
     direction = STOP;
-    snakeX = rand() % (boardSizeX - 2) + 1; //without the first and last line
+    snakeX = rand() % (boardSizeX - 2) + 1; //snakes spawns randomly within the boarder
     snakeY = rand() % (boardSizeY - 2) + 1;
     appleX = rand() % (boardSizeX - 2) + 1;
     appleY = rand() % (boardSizeY - 2) + 1;
@@ -31,27 +33,28 @@ void Start()
 
 void Drawing()
 {
+
     system("cls");  //clears the screen before drawing
 
     for (int x = 0; x <= boardSizeX; x++)
-        cout << '#';
+        cout << 'X';
     cout << endl;
 
     for (int y = 1; y < boardSizeY; y ++)   
     {
         for (int x = 0; x <= boardSizeX; x ++)
         {
-            if (x == 0 || x == boardSizeX) cout << '#';
+            if (x == 0 || x == boardSizeX) cout << 'X';
             else if (x == snakeX && y == snakeY)
             {
-                changeColor(2);
+                changeColor(2);     //draws head of the snake
                 cout << 'O';
                 changeColor(7);
             }
             else if (x == appleX && y == appleY)
             {
-                changeColor(12);
-                cout << 'A';
+                changeColor(12);        //draws the 'apple'
+                cout << '*';
                 changeColor(7);
             }
             else
@@ -62,7 +65,7 @@ void Drawing()
                 {
                     if (snakebodyX[i] == x && snakebodyY[i] == y)
                     {
-                        changeColor(2);
+                        changeColor(2);     //draws the tail of the snake 
                         cout << 'o';
                         body = true;
                         changeColor(7);
@@ -79,9 +82,8 @@ void Drawing()
     }
 
     for (int x = 0; x <= boardSizeX; x++)
-        cout << '#';
+        cout << 'X';
     cout << endl;
-
 }
 
 void Controling()
@@ -108,7 +110,7 @@ void Gameplay()
     snakebodyX[0] = snakeX;
     snakebodyY[0] = snakeY;
 
-    for (int i = 1; i < bodynumber; i++)
+    for (int i = 1; i < bodynumber; i++)            //spawning snakes body
     {
         lastsnakeX2 = snakebodyX[i];
         lastsnakeY2 = snakebodyY[i];
@@ -128,28 +130,36 @@ void Gameplay()
     default: break;
     }
 
-    if (snakeX >= boardSizeX) snakeX = 1;
+    if (snakeX >= boardSizeX) snakeX = 1;       //going into the boarder
     if (snakeX <= 0) snakeX = boardSizeX - 1;
     if (snakeY >= boardSizeY) snakeY = 1;
     if (snakeY <= 0) snakeY = boardSizeY - 1;
 
-    if (snakeX == appleX && snakeY == appleY)
+    if (snakeX == appleX && snakeY == appleY)       //spawning apple
     {
         bodynumber++;
         appleX = rand() % (boardSizeX - 2) + 1;
         appleY = rand() % (boardSizeY - 2) + 1;
+        score++;
        
     }
 
     for (int i = 0; i < bodynumber; i++)    //gameover if snake crosses its tail
+    {
         if (snakeX == snakebodyX[i] && snakeY == snakebodyY[i])
             gameStop = true;
+        if (snakebodyX[i] == appleX && snakebodyY[i] == appleY) //if the apple spawns in snakes body it respawns somwhere else
+        {
+            appleX = rand() % (boardSizeX - 2) + 1;
+            appleY = rand() % (boardSizeY - 2) + 1;
+        }
+    }
 
 }
 
 int main()
 {
-    
+      
     Start();
     while (gameStop != true)
     {
@@ -158,7 +168,9 @@ int main()
         Gameplay();
         Sleep(70);  //function to make the snake slower
     }
+    
+    cout << "\n\n\tYour score: " << score << "\n\n";
 
     
-    return 0;
+    return 0; 
 }
